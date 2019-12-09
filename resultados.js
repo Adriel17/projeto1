2bbfdd8917a3ec4db0d40a8dbf2730d61f036091
Vinicius Ricardo da Silva <vinicius.ricardo@academico.ifrn.edu.br>
2019-12-09 16:46:35 -0300

    90% das abas prontas

--- resultados.js
+++ resultados.js
@@ -1,15 +1,29 @@
 
 function verificar(){
     var email = $("#email").val();
+    var senha = $("#senha").val();
+    
     console.log(email);
     var lista = ["vinicius@gmail.com", "adriel@hotmail.com"];
-    for(i = 0; i < lista.length - 1; i++){
-    if(email == lista[0]){
+    var pass = ["vb729401", "adriel4578"];
+
+     
+    var email1 = $('#cdemail').val();
+    var senha1 = $("#cdsenha").val();
+    console.log(email1);
+
+    lista.push(email1);
+    pass.push(senha1);
+   
+
+
+    if(email == lista[0] && senha == pass[0]){
         window.location.href = "arquivos.html";
-    } else if(email == lista[1]) {
+    } else if(email == lista[2] && senha[2] == pass[2]) {
         window.location.href = "arquivos1.html";
+    } else {
+        $('#aviso').html("<p class='red-text text-accent-2'>Dados incorretos. Verifique seu email e senha!</p>");
     }
-}
     
     /*var exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
     var check=/@[w-]+./;
@@ -17,4 +31,6 @@ function verificar(){
     if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
     else {return window.location.href = "inicial.html";}*/
 }
+
+
 
