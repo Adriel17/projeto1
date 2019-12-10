524b4d786bebd8e217bc231a63623aa5bce52945
Vinicius Ricardo da Silva <vinicius.ricardo@academico.ifrn.edu.br>
2019-12-10 12:58:41 -0300

    Corrigindo bug ((cadastro))

--- resultados.js
+++ resultados.js
@@ -1,25 +1,16 @@
 
+
 function verificar(){
-    var email = $("#email").val();
-    var senha = $("#senha").val();
-    
-    console.log(email);
     var lista = ["vinicius@gmail.com", "adriel@hotmail.com"];
     var pass = ["vb729401", "adriel4578"];
+    var email = $("#email").val();
+    var senha = $("#senha").val();
 
-     
-    var email1 = $('#cdemail').val();
-    var senha1 = $("#cdsenha").val();
-    console.log(email1);
-
-    lista.push(email1);
-    pass.push(senha1);
-   
 
 
     if(email == lista[0] && senha == pass[0]){
         window.location.href = "arquivos.html";
-    } else if(email == lista[2] && senha[2] == pass[2]) {
+    } else if(email == lista[1] && senha == pass[1]) {
         window.location.href = "arquivos1.html";
     } else {
         $('#aviso').html("<p class='red-text text-accent-2'>Dados incorretos. Verifique seu email e senha!</p>");
@@ -31,6 +22,5 @@ function verificar(){
     if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
     else {return window.location.href = "inicial.html";}*/
 }
-
 
 
